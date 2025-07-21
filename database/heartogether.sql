4cd82348001d9606b1a7520c57de020b745e35bf
soohong-26 <TSHplays@gmail.com>
2025-07-21 11:26:56 +0800

    minor updated to the database

--- database/heartogether.sql
+++ database/heartogether.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2025 at 07:08 AM
+-- Generation Time: Jul 21, 2025 at 05:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,7 +44,7 @@ INSERT INTO `faq` (`id`, `category`, `question`, `answer`, `category_id`) VALUES
 (21, 'Basics of Hearing Impairment', 'What device can help people with hearing loss hear better?', 'A hearing aid is a small electronic device that helps people with hearing loss hear more clearly by amplifying sound.', NULL),
 (22, 'Basics of Hearing Impairment', 'What does a hearing aid do?', 'A hearing aid helps people with hearing loss hear better by making sounds louder and clearer.', NULL),
 (23, 'Deaf Culture & Communication', 'What is sign language primarily used for?', 'Sign language is mainly used as a way for people who are deaf or hard of hearing to communicate with others using hand signs and gestures.', NULL),
-(24, 'Deaf Culture & Communication', 'What is deaf awareness?', 'Deaf awareness means understanding and respecting the needs, culture, and experiences of people who are deaf or hard of hearing.', NULL),
+(24, 'Deaf Culture & Communication', 'What is deaf awareness?', 'Deaf awareness is about understanding, respecting, and supporting the experiences of people who are deaf or hard of hearing. It’s not just about knowing that hearing loss exists—it\'s about creating a more inclusive and accessible world where deaf individuals can fully participate in daily life, education, work, and social interactions.', NULL),
 (25, 'Deaf Culture & Communication', 'What is fingerspelling?', 'Fingerspelling is a part of sign language where each letter of a word is spelled out using hand signs.', NULL),
 (26, 'Best Practices for Communicating', 'Why is facial expression important in sign language?', 'Facial expressions show emotions and context, making the meaning of the signs clearer during communication.', NULL),
 (27, 'Best Practices for Communicating', 'Which hand can you use in sign language?', 'You can use either hand in sign language, but it\'s important to be consistent and comfortable with your choice.', NULL),
@@ -55,7 +55,9 @@ INSERT INTO `faq` (`id`, `category`, `question`, `answer`, `category_id`) VALUES
 (32, 'Best Practices for Communicating', 'Which part of the body does hearing mostly involve?', 'Hearing mainly involves the ears.', NULL),
 (33, 'Practical Communication Tips', 'Which of the following is a sign language greeting?', 'Waving is a common way to greet someone in sign language.', NULL),
 (34, 'General Advice', 'Why is it important to be deaf aware?', 'Being deaf aware means being considerate and respectful, which makes communication and interaction easier and more inclusive for everyone.', NULL),
-(35, 'General Advice', 'What should I do if I feel overwhelmed while supporting someone with hearing loss?', 'Feeling overwhelmed is normal when learning how to support someone with hearing loss. Remember to take things step by step, seek support from family, friends, or local organisations, and don’t hesitate to consult with professionals if needed. It’s essential to look after your well-being, too—take breaks, ask questions, and know it’s okay to seek help along the way.', NULL);
+(35, 'General Advice', 'What should I do if I feel overwhelmed while supporting someone with hearing loss?', 'Feeling overwhelmed is normal when learning how to support someone with hearing loss. Remember to take things step by step, seek support from family, friends, or local organisations, and don’t hesitate to consult with professionals if needed. It’s essential to look after your well-being, too—take breaks, ask questions, and know it’s okay to seek help along the way.', NULL),
+(36, 'Deaf Culture & Communication', 'Why is deaf awareness important?', 'Many deaf people face barriers that others might not even notice—like lack of captions on videos, poor lighting for sign language communication, or being spoken to from behind. Deaf awareness encourages us to change our environments, attitudes, and communication methods to break down those barriers. It promotes equal opportunities, helps reduce stigma, and ensures deaf people feel seen, heard, and valued.', NULL),
+(37, 'Deaf Culture & Communication', 'How can I be more deaf aware?', 'Learn basic sign language – Even a few signs like “hello” or “thank you” can go a long way.\r\n\r\nFace the person when speaking – This helps with lip-reading and connection.\r\n\r\nUse visual cues – Flashing lights, captions, or text messages can be more helpful than sound.\r\n\r\nBe patient and respectful – Communication might take more effort, but it’s always worth it.\r\n\r\nAvoid assumptions – Not all deaf people use the same communication methods; some use speech, some use sign, and some use both.', NULL);
 
 -- --------------------------------------------------------
 
@@ -107,7 +109,9 @@ INSERT INTO `quiz_attempts` (`attempt_id`, `username`, `score`, `attempt_date`) 
 (6, 'PenguinPlays', 1, '2025-06-12 09:08:05'),
 (7, 'Testuser', 5, '2025-06-17 09:46:25'),
 (8, 'Testuser', 5, '2025-06-17 09:46:37'),
-(9, 'Testuser', 5, '2025-06-17 09:47:12');
+(9, 'Testuser', 5, '2025-06-17 09:47:12'),
+(10, 'PenguinPlays', 5, '2025-07-17 17:17:02'),
+(11, 'PenguinPlays', 1, '2025-07-21 08:55:01');
 
 -- --------------------------------------------------------
 
@@ -147,7 +151,12 @@ INSERT INTO `quiz_questions` (`question_id`, `question_text`, `image`, `option_a
 (24, 'What sign is this?', 'uploads/6850c17bad48b_hello.gif', 'Goodbye', 'Hello', 'Thank you', 'Yes', 'B'),
 (25, 'What sign is this?', 'uploads/6850c19c09e7f_thank_you.gif', 'Hello', 'Please', 'Thank you', 'Yes', 'C'),
 (26, 'What sign is this?', 'uploads/6850c1bd193f9_how_are_you.gif', 'No', 'Goodbye', 'Yes', 'How are you?', 'D'),
-(27, 'What sign is this?', 'uploads/6850c1d6170fb_sorry.gif', 'Thank you', 'Hello', 'Sorry', 'Goodbye', 'C');
+(27, 'What sign is this?', 'uploads/6850c1d6170fb_sorry.gif', 'Thank you', 'Hello', 'Sorry', 'Goodbye', 'C'),
+(29, 'What does \"Deaf Awareness\" primarily aim to promote?', NULL, 'Teaching people how to fix hearing aids', 'Avoiding communication with deaf individuals', 'Understanding and supporting the deaf community', 'Preventing sign language use', 'C'),
+(30, 'What is one common communication barrier for deaf individuals in public places?', NULL, 'Overcrowding', 'Loud background music', 'Lack of visual information', 'Free Wi-Fi', 'C'),
+(31, 'What is the most appropriate way to communicate with a deaf person who uses sign language?', NULL, 'Speak slowly and louder', 'Write everything down', 'Use gestures or sign language', 'Avoid communication altogether', 'C'),
+(32, 'Which one of these is a widely used form of communication in the deaf community?', NULL, 'Morse code', 'Sign language', 'Lip reading', 'Braille', 'B'),
+(33, 'Why is learning some basic sign language beneficial?', NULL, 'Only deaf people need to learn it', 'It helps in loud environments', 'It\'s trendy on social media', 'It promotes inclusivity and better communication', 'D');
 
 -- --------------------------------------------------------
 
@@ -196,7 +205,17 @@ INSERT INTO `quiz_responses` (`response_id`, `attempt_id`, `question_id`, `selec
 (29, 9, 24, 'B', 1),
 (30, 9, 14, 'C', 1),
 (31, 9, 2, 'B', 1),
-(32, 9, 18, 'B', 1);
+(32, 9, 18, 'B', 1),
+(33, 10, 15, 'A', 1),
+(34, 10, 8, 'C', 1),
+(35, 10, 24, 'B', 1),
+(36, 10, 4, 'C', 1),
+(37, 10, 9, 'B', 1),
+(38, 11, 6, 'A', 0),
+(39, 11, 12, 'A', 1),
+(40, 11, 27, 'A', 0),
+(41, 11, 18, 'A', 0),
+(42, 11, 13, 'A', 0);
 
 -- --------------------------------------------------------
 
@@ -211,20 +230,21 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `profile_img` varchar(255) NOT NULL,
   `roles` varchar(10) NOT NULL,
-  `is_approved` tinyint(1) NOT NULL DEFAULT 0
+  `is_approved` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `profile_img`, `roles`, `is_approved`) VALUES
-(1, 'soohong', '$2y$10$7Aw3ek/m3QfZDMxNhflVRuWvSoe3p1.1YZtVKOm.OeDfJCGZ9nZYS', 'TSHplays@gmail.com', 'profile/soohong_683d9be6649b4.png', 'admin', 1),
-(2, 'PenguinPlays', '$2y$10$S5tU1WCjMV.Dfjip.mPum.lu04y6srQECAQdlICZc5X9R.kU8.77e', 'soohong@gmail.com', 'profile/PenguinPlays_683d9b78e2b26.jpg', 'user', 1),
-(8, 'testing', '$2y$10$NzCkfpSTjsxbv94OEu0RfuCIlWgzs8jTclQ6p90/xnNSmFBjGdTpe', 'testing@gmail.com', 'profile/profile.png', 'user', 0),
-(9, 'soohong1', '$2y$10$bdWyA89ZLAv1AxUDuc4cb.qEuHq7QihOrEthD1eHv2MDt2UziHEM2', 'soohongwastaken@gmail.com', 'profile/profile.png', 'user', 1),
-(10, '123', '$2y$10$ecFcwpxN7BiMlfFEGLst6e0TXa.8Y.Iak.Mz5c9jtBrS8zNcYv5ya', 'gerg@gmail.com', 'profile/profile.png', 'user', 0),
-(11, 'Testuser', '$2y$10$G3DoYMMIL/GJb6.breU5mOoA63O7cbWbev6Jk5RJMlpGHXd6/JicC', 'test@gmail.com', 'profile/profile.png', 'user', 1);
+INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `profile_img`, `roles`, `is_approved`, `created_at`) VALUES
+(1, 'soohong', '$2y$10$7Aw3ek/m3QfZDMxNhflVRuWvSoe3p1.1YZtVKOm.OeDfJCGZ9nZYS', 'TSHplays@gmail.com', 'profile/soohong_683d9be6649b4.png', 'admin', 1, '2025-07-17 10:55:21'),
+(2, 'PenguinPlays', '$2y$10$S5tU1WCjMV.Dfjip.mPum.lu04y6srQECAQdlICZc5X9R.kU8.77e', 'soohong@gmail.com', 'profile/PenguinPlays_683d9b78e2b26.jpg', 'user', 1, '2025-06-17 10:55:21'),
+(8, 'testing', '$2y$10$NzCkfpSTjsxbv94OEu0RfuCIlWgzs8jTclQ6p90/xnNSmFBjGdTpe', 'testing@gmail.com', 'profile/profile.png', 'user', 0, '2025-07-17 10:55:21'),
+(9, 'soohong1', '$2y$10$bdWyA89ZLAv1AxUDuc4cb.qEuHq7QihOrEthD1eHv2MDt2UziHEM2', 'soohongwastaken@gmail.com', 'profile/profile.png', 'user', 1, '2025-07-17 10:55:21'),
+(10, '123', '$2y$10$ecFcwpxN7BiMlfFEGLst6e0TXa.8Y.Iak.Mz5c9jtBrS8zNcYv5ya', 'gerg@gmail.com', 'profile/profile.png', 'user', 0, '2025-05-17 10:55:21'),
+(11, 'Testuser', '$2y$10$G3DoYMMIL/GJb6.breU5mOoA63O7cbWbev6Jk5RJMlpGHXd6/JicC', 'test@gmail.com', 'profile/profile.png', 'user', 1, '2025-07-17 10:55:21');
 
 -- --------------------------------------------------------
 
@@ -290,7 +310,27 @@ INSERT INTO `videos` (`video_id`, `filename`, `title`, `category`) VALUES
 (64, 'meat.gif', 'Meat', 'Food'),
 (65, 'chocolate.gif', 'Chocolate', 'Food'),
 (66, 'burger.gif', 'Burger', 'Food'),
-(67, 'biscuits.gif', 'Biscuits', 'Food');
+(67, 'biscuits.gif', 'Biscuits', 'Food'),
+(68, 'ambition.gif', 'Ambition', 'Actions'),
+(69, 'physical.gif', 'Physical', 'Actions'),
+(70, 'focus.gif', 'Focus', 'Actions'),
+(71, 'quality.gif', 'Quality', 'Actions'),
+(72, 'advice.gif', 'Advice', 'Actions'),
+(73, 'fair.gif', 'Fair', 'Adjectives'),
+(74, 'trust.gif', 'Trust', 'Adjectives'),
+(75, 'peaceful.gif', 'Peaceful', 'Adjectives'),
+(76, 'nice.gif', 'Nice', 'Adjectives'),
+(77, 'dangerous.gif', 'Dangerous', 'Adjectives'),
+(78, 'beer.gif', 'Beer', 'Beverages'),
+(79, 'coke.gif', 'Coke', 'Beverages'),
+(80, 'coffee.gif', 'Coffee', 'Beverages'),
+(81, 'Milo.gif', 'Milo', 'Beverages'),
+(82, 'syrup.gif', 'Syrup', 'Beverages'),
+(83, 'what.gif', 'What', 'Questions'),
+(84, 'why.gif', 'Why', 'Questions'),
+(85, 'how.gif', 'How', 'Questions'),
+(86, 'when.gif', 'When', 'Questions'),
+(87, 'where.gif', 'Where', 'Questions');
 
 -- --------------------------------------------------------
 
@@ -309,10 +349,14 @@ CREATE TABLE `video_categories` (
 --
 
 INSERT INTO `video_categories` (`id`, `name`, `category_order`) VALUES
-(19, 'Alphabets', 0),
-(186, 'Greetings', 1),
-(229, 'Taste', 0),
-(243, 'Food', 0);
+(19, 'Alphabets', 1),
+(186, 'Greetings', 4),
+(229, 'Taste', 3),
+(243, 'Food', 0),
+(269, 'Actions', 2),
+(290, 'Adjectives', 0),
+(305, 'Beverages', 0),
+(322, 'Questions', 0);
 
 -- --------------------------------------------------------
 
@@ -407,31 +451,31 @@ ALTER TABLE `website_ratings`
 -- AUTO_INCREMENT for table `faq`
 --
 ALTER TABLE `faq`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `faq_categories`
 --
 ALTER TABLE `faq_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=150;
 
 --
 -- AUTO_INCREMENT for table `quiz_attempts`
 --
 ALTER TABLE `quiz_attempts`
-  MODIFY `attempt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `attempt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `quiz_questions`
 --
 ALTER TABLE `quiz_questions`
-  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `quiz_responses`
 --
 ALTER TABLE `quiz_responses`
-  MODIFY `response_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `response_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -443,13 +487,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `videos`
 --
 ALTER TABLE `videos`
-  MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
 
 --
 -- AUTO_INCREMENT for table `video_categories`
 --
 ALTER TABLE `video_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=265;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=338;
 
 --
 -- AUTO_INCREMENT for table `website_ratings`
